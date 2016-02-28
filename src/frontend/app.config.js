8e857b4a0ad614d907862e499fa2623f69c567a5
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-02-29 01:36:23 +0300

    added button 2

--- src/frontend/app.config.js
+++ src/frontend/app.config.js
@@ -13,22 +13,49 @@ module.exports = function (ngApp) {
             // home route
             .state('home', {
                 url: "/home",
-                template: require('./templates/pages/home.html')
+                templateUrl: './templates/pages/home.html'
             })
 
             .state('home.modal', {
                 url: "/:id",
-                //template: require('./templates/pages/modal'+ $stateParams.id +'.html'),
                 onEnter: ['$stateParams', '$state', function ($stateParams, $state) {
-                    $('.ui.modal')
+                    let modalWindow = $('.ui.modal');
+
+                    modalWindow
                         .modal({
                             onHide: function () {
+                                // after close modal go back to previous route
                                 $state.go('^');
+                                console.log('onHide');
+                            },
+                            onShow: function () {
+                                // loading data
+                                $.ajax({
+                                    url: './templates/content/' + $stateParams.id + '.html',
+                                    statusCode: {
+                                        // if content page not exist
+                                        404: function () {
+                                            modalWindow.
+                                                children('.content').html('<div class="ui red message">Sorry, but content file not found!</div>');
+                                        }
+                                    }
+                                }).done(function (data) {
+                                    // load data from content file
+                                    modalWindow.children('.content').html(data);
+                                });
                             }
                         })
-                        .modal('show')
-                    ;
-                }]
+                        .modal('show');
+
+                    console.log('modal');
+                }],
+                onExit: function () {
+                }
+            })
+            // help route
+            .state('help', {
+                url: "/help",
+                template: require('./templates/pages/help.html')
             })
         ;
     }
