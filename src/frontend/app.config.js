dfce678cb7528982d824ff203cb7c02212aaf749
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-02-28 19:27:59 +0300

    changed routing config

--- src/frontend/app.config.js
+++ src/frontend/app.config.js
@@ -15,15 +15,42 @@ module.exports = function (ngApp) {
                 url: "/home",
                 template: require('./templates/pages/home.html')
             })
+
+            .state('home.modal1', {
+                url: "/modal1",
+                templateUrl: '/',
+                onEnter: ['$stateParams', '$state', function ($stateParams, $state) {
+                    $('.ui.modal')
+                        .modal({
+                            onHide: function () {
+                                $state.go('^');
+                            }
+                        })
+                        .modal('show')
+                    ;
+                }]
+
+            })
+
             .state('modal', {
                 url: "/modal",
                 template: require('./templates/pages/modal.html')
             })
+
             .state('modal.modal1', {
                 url: "/modal1",
-                onEnter: ['$stateParams', '$state', function ($stateParams, $state, $uibModal) {
-                    $('.ui.modal').modal('show');
+                templateUrl: '/',
+                onEnter: ['$stateParams', '$state', function ($stateParams, $state) {
+                    $('.ui.modal')
+                        .modal({
+                            onHide: function () {
+                                $state.go('^');
+                            }
+                        })
+                        .modal('show')
+                    ;
                 }]
+
             })
         ;
     }
