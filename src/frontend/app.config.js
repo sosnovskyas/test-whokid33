3b57f63db2b794e80ec5574f124e977cb5f0ba83
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-02-28 20:03:48 +0300

    added set params use

--- src/frontend/app.config.js
+++ src/frontend/app.config.js
@@ -16,9 +16,9 @@ module.exports = function (ngApp) {
                 template: require('./templates/pages/home.html')
             })
 
-            .state('home.modal1', {
-                url: "/modal1",
-                templateUrl: '/',
+            .state('home.modal', {
+                url: "/:id",
+                //template: require('./templates/pages/modal'+ $stateParams.id +'.html'),
                 onEnter: ['$stateParams', '$state', function ($stateParams, $state) {
                     $('.ui.modal')
                         .modal({
@@ -29,28 +29,6 @@ module.exports = function (ngApp) {
                         .modal('show')
                     ;
                 }]
-
-            })
-
-            .state('modal', {
-                url: "/modal",
-                template: require('./templates/pages/modal.html')
-            })
-
-            .state('modal.modal1', {
-                url: "/modal1",
-                templateUrl: '/',
-                onEnter: ['$stateParams', '$state', function ($stateParams, $state) {
-                    $('.ui.modal')
-                        .modal({
-                            onHide: function () {
-                                $state.go('^');
-                            }
-                        })
-                        .modal('show')
-                    ;
-                }]
-
             })
         ;
     }
